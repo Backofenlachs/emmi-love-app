b510a38e50d3daef94346e566556c72cd0597723
Backofenlachs <perseus.palma-jacobs@outlook.com>
2026-08-17 20:44:17 +0200

    feat: handle web push notifications

--- vite.config.js
+++ vite.config.js
@@ -40,7 +40,8 @@ export default defineConfig({
       },
 
       workbox: {
-        globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+        globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+        importScripts: ['push-handler.js']
       }
     })
   ],
